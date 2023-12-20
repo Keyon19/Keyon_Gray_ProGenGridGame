a48582d12183ec78a9d28e7fb06d300882f6df76
Keyon19 <keyon.gray@mail.citytech.cuny.edu>
2023-12-19 23:06:37 -0500

    Updated

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
-    public Timer timer;
+    //public Timer timer;
+    public PlayerControl playerControl;
+
+    public float timeStart;
+    public bool timeActive = false;
+
+    public TextMeshProUGUI TimerText;
+
+    /*
     void Awake()
     {
         if (instance != null)
@@ -19,19 +29,40 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
     }
-
+*/
     void Start()
     {
         //timer.PauseTimer();
-        
+        timeActive = true;
     }
 
     
     void Update()
     {
-        
+        if (timeActive)
+        {
+            if (timeStart > 0)
+            {
+                timeStart -= Time.deltaTime;
+                updateTimer(timeStart);
+            }
+            else
+            {
+                timeStart = 0;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
     }
 
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        TimerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
 
 
     public void RestartGame()
